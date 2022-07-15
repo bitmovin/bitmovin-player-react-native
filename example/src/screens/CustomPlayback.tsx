fbcda21668c79340790c973e70cf3037e8a7bcf1
pogist <murilo.paixao.2@gmail.com>
2022-07-15 14:40:23 -0300

    feat(example): add custom playback player screen

---
+++ example/src/screens/CustomPlayback.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { usePlayer, PlayerView } from 'bitmovin-player-react-native';
+import { RootStackParamsList } from '../App';
+
+type CustomPlaybackProps = NativeStackScreenProps<
+  RootStackParamsList,
+  'CustomPlayback'
+>;
+
+const CustomPlayback: React.FC<CustomPlaybackProps> = ({ route }) => {
+  const player = usePlayer({
+    licenseKey: route.params?.licenseKey,
+  });
+
+  useFocusEffect(
+    useCallback(() => {
+      const { streamURL, streamType } = route.params;
+      if (streamURL && streamType) {
+        player.load({
+          url: streamURL,
+          type: streamType.value,
+        });
+      }
+      return () => {
+        player.destroy();
+      };
+    }, [player, route.params])
+  );
+
+  return (
+    <View style={styles.container}>
+      <PlayerView style={styles.player} player={player} />
+    </View>
+  );
+};
+
+export default CustomPlayback;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  player: {
+    flex: 1,
+  },
+});
