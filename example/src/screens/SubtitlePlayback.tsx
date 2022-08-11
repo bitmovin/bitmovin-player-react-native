607161a5b105bc6d37efa0db7554d3986c485df7
pogist <murilo.paixao.2@gmail.com>
2022-08-11 20:35:04 -0300

    feat(example/subtitles): add basic subtitle example screen code

--- example/src/screens/SubtitlePlayback.tsx
+++ example/src/screens/SubtitlePlayback.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, Platform, StyleSheet } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import {
+  Event,
+  usePlayer,
+  PlayerView,
+  SourceType,
+} from 'bitmovin-player-react-native';
+
+function prettyPrint(header: string, obj: any) {
+  console.log(header, JSON.stringify(obj, null, 2));
+}
 
 export default function SubtitlePlayback() {
+  const player = usePlayer();
+
+  useFocusEffect(
+    useCallback(() => {
+      player.load({
+        url:
+          Platform.OS === 'ios'
+            ? 'https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+            : 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd',
+        type: Platform.OS === 'ios' ? SourceType.HLS : SourceType.DASH,
+        poster: 'https://bitmovin-a.akamaihd.net/content/sintel/poster.png',
+      });
+      return () => {
+        player.destroy();
+      };
+    }, [player])
+  );
+
+  const onReady = useCallback((event: Event) => {
+    prettyPrint(`EVENT [${event.name}]`, event);
+  }, []);
+
+  const onEvent = useCallback((event: Event) => {
+    prettyPrint(`EVENT [${event.name}]`, event);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.warningText}>
-        🚧 This feature is currently a work in progress
-      </Text>
+      <PlayerView
+        player={player}
+        style={styles.player}
+        onPlay={onEvent}
+        onPlaying={onEvent}
+        onPaused={onEvent}
+        onReady={onReady}
+        onSourceLoaded={onEvent}
+        onSeek={onEvent}
+        onSeeked={onEvent}
+      />
     </View>
   );
 }
@@ -16,10 +61,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'black',
   },
-  warningText: {
-    color: 'black',
-    fontSize: 20,
+  player: {
+    flex: 1,
   },
 });
