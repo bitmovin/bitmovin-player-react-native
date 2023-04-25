ffd2f0cd3ecfa6227ca5d42e2c991b27bf31f5cc
Matthias Tamegger <matamegger@gmail.com>
2023-04-25 16:18:53 +0200

    chore(android): add extension for ReadableMap

---
+++ android/src/main/java/com/bitmovin/player/reactnative/extensions/ReadableMapExtension.kt
@@ -0,0 +1,8 @@
+package com.bitmovin.player.reactnative.extensions
+
+import com.facebook.react.bridge.ReadableMap
+
+fun ReadableMap.getBooleanOrNull(
+    key: String
+): Boolean? = takeIf { hasKey(key) }
+    ?.getBoolean(key)
