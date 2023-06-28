1b2b1067566fef34c4961425b0aea7e78a908186
Žiga Vehovec <ziga.vehovec@bitmovin.com>
2023-06-28 15:12:33 +0200

    Update src/player.ts
    Co-authored-by: Roland Kákonyi <roland.kakonyi@bitmovin.com>

--- src/player.ts
+++ src/player.ts
@@ -425,7 +425,7 @@ export class Player extends NativeInstance<PlayerConfig> {
    * The limit in seconds for time shifting. This value is either negative or 0 and it is always 0 if the active
    * `source` is not a live stream or there is no active playback session.
    */
-  getMaxTimeShift = (): Promise<boolean> => {
+  getMaxTimeShift = async (): Promise<number> => {
     return PlayerModule.getMaxTimeShift(this.nativeId);
   };
 }
