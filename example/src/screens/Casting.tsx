2c57d12af5584640aecd7b19b1977580466fb886
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2023-09-21 14:04:08 +0200

    feat(casting): cast DASH source from iOS

--- example/src/screens/Casting.tsx
+++ example/src/screens/Casting.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import {
   Event,
@@ -7,6 +7,7 @@ import {
   PlayerView,
   SourceType,
   BitmovinCastManager,
+  Source,
 } from 'bitmovin-player-react-native';
 
 function prettyPrint(header: string, obj: any) {
@@ -20,11 +21,29 @@ export default function Casting() {
 
   useFocusEffect(
     useCallback(() => {
-      player.load({
-        url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8',
-        type: SourceType.HLS,
-        title: 'BipBop - Apple sample stream',
+      const source = new Source({
+        url:
+          Platform.OS === 'ios'
+            ? 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
+            : 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+        type: Platform.OS === 'ios' ? SourceType.HLS : SourceType.DASH,
+        title: 'Art of Motion',
+        poster:
+          'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg',
+        thumbnailTrack:
+          'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/thumbnails/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.vtt',
+        metadata: { platform: Platform.OS },
       });
+
+      // Configure playing DASH source on Chromecast, even when casting from iOS.
+      source.remotePlayback = {
+        castSourceConfig: {
+          url: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+          type: SourceType.DASH,
+          title: 'Art of Motion',
+        },
+      };
+      player.loadSource(source);
       return () => {
         player.destroy();
       };
