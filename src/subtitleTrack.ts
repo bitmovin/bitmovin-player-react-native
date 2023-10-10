c73c55d7196ee4b0dbc84f3f22a75a75f274b7f0
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2023-10-10 15:00:46 +0200

    feat(apidocs): simplify SideLoadedSubtitleTrack definition

--- src/subtitleTrack.ts
+++ src/subtitleTrack.ts
@@ -49,37 +49,9 @@ export interface SubtitleTrack {
  * A subtitle track that can be added to `SourceConfig.subtitleTracks`.
  *
  */
-export interface SideLoadedSubtitleTrack {
-  /**
-   * The URL to the timed file, e.g. WebVTT file.
-   */
+export interface SideLoadedSubtitleTrack extends SubtitleTrack {
   url: string;
-  /**
-   * The label for this track.
-   */
   label: string;
-  /**
-   * The IETF BCP 47 language tag associated with this track, e.g. `pt`, `en`, `es` etc.
-   */
   language: string;
-  /**
-   * The unique identifier for this track. If no value is provided, a random UUIDv4 will be generated for it.
-   */
-  identifier?: string;
-  /**
-   * Specifies the file format to be used by this track.
-   */
   format: SubtitleFormat;
-  /**
-   * If set to true, this track would be considered as default. Default is `false`.
-   */
-  isDefault?: boolean;
-  /**
-   * Tells if a subtitle track is forced. If set to `true` it means that the player should automatically
-   * select and switch this subtitle according to the selected audio language. Forced subtitles do
-   * not appear in `Player.getAvailableSubtitles`.
-   *
-   * Default is `false`.
-   */
-  isForced?: boolean;
 }
