064a95bbedc982bd2ad9857ed27034232f5eff7a
Lukas Knoch-Girstmair <strangesource@users.noreply.github.com>
2023-10-18 17:10:29 +0200

    Update src/components/PlayerView/playerViewConfig.ts
    Co-authored-by: Matthias Tamegger <matamegger@users.noreply.github.com>

--- src/components/PlayerView/playerViewConfig.ts
+++ src/components/PlayerView/playerViewConfig.ts
@@ -9,7 +9,7 @@ export interface PlayerViewConfig {
    * Default is {@link WebUiConfig}.
    *
    * Limitations:
-   * Configuring the `uiConfig` only has effect if the {@link StyleConfig.userInterfaceType} is set to {@link UserInterfaceType.Bitmovin}.
+   * Configuring the `uiConfig` only has an effect if the {@link StyleConfig.userInterfaceType} is set to {@link UserInterfaceType.Bitmovin}.
    */
   uiConfig: UiConfig;
 }
