65c0eb38eabe7cc914dc8c735fd06750d9055670
Michele Pozzi <123.mpozzi@gmail.com>
2023-11-03 09:41:11 +0100

    feat(playbackSpeed): add tvOS doc note

--- src/player.ts
+++ src/player.ts
@@ -50,7 +50,7 @@ export class Player extends NativeInstance<PlayerConfig> {
    * @note
    * - Slow motion is used by values between `0` and `1`.
    * - Fast forward by values greater than `2`.
-   * - Slow reverse is used by values between `0` and `-1`, and fast reverse is used by values less than `-1`, both are iOS-only. //TODO: tvOS too?
+   * - Slow reverse is used by values between `0` and `-1`, and fast reverse is used by values less than `-1`. iOS and tvOS only.
    * @note
    * Negative values are ignored during Casting.
    * @note
