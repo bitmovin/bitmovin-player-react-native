d3c9572e533825451b9b995202b91c471cab2167
Kevin Rocard <kevin.rocard@bitmovin.com>
2023-11-14 15:20:42 +0100

    refactor: rename PromiseRejectOnExceptionBlock to RejectPromiseOnExceptionBlock

--- android/src/main/java/com/bitmovin/player/reactnative/BitmovinBaseModule.kt
+++ android/src/main/java/com/bitmovin/player/reactnative/BitmovinBaseModule.kt
@@ -1,6 +1,7 @@
 package com.bitmovin.player.reactnative
 
 import com.bitmovin.player.api.Player
+import com.bitmovin.player.reactnative.extensions.drmModule
 import com.bitmovin.player.reactnative.extensions.offlineModule
 import com.bitmovin.player.reactnative.extensions.playerModule
 import com.bitmovin.player.reactnative.extensions.sourceModule
@@ -9,19 +10,20 @@ import com.facebook.react.bridge.*
 import com.facebook.react.uimanager.UIManagerModule
 
 /**
- * Base for Bitmovin React Module.
+ * Base for Bitmovin React modules.
  *
  * Provides many helper methods that are promise exception safe.
  *
  * In general, code should not throw while resolving a [Promise]. Instead, [Promise.reject] should be used.
  * This doesn't match Kotlin's error style, which uses exception. The helper methods in this class, provide such
  * convenience, they can only be called in a context that will catch any Exception and reject the [Promise].
+ *
  */
 abstract class BitmovinBaseModule(
     protected val context: ReactApplicationContext,
 ) : ReactContextBaseJavaModule(context) {
     /** [resolve] the [Promise] by running [block] in the UI thread with [UIManagerModule.addUIBlock].  */
-    protected inline fun <T> Promise.resolveOnUIThread(crossinline block: PromiseRejectOnExceptionBlock.() -> T) {
+    protected inline fun <T> Promise.resolveOnUIThread(crossinline block: RejectPromiseOnExceptionBlock.() -> T) {
         val uiManager = runAndRejectOnException { uiManager } ?: return
         uiManager.addUIBlock {
             runAndRejectOnException {
@@ -31,29 +33,32 @@ abstract class BitmovinBaseModule(
         }
     }
 
-    protected val PromiseRejectOnExceptionBlock.playerModule: PlayerModule get() = context.playerModule
+    protected val RejectPromiseOnExceptionBlock.playerModule: PlayerModule get() = context.playerModule
         ?: throw IllegalArgumentException("PlayerModule not found")
 
-    protected val PromiseRejectOnExceptionBlock.uiManager: UIManagerModule get() = context.uiManagerModule
+    protected val RejectPromiseOnExceptionBlock.uiManager: UIManagerModule get() = context.uiManagerModule
         ?: throw IllegalStateException("UIManager not found")
 
-    protected val PromiseRejectOnExceptionBlock.sourceModule: SourceModule get() = context.sourceModule
+    protected val RejectPromiseOnExceptionBlock.sourceModule: SourceModule get() = context.sourceModule
         ?: throw IllegalStateException("SourceModule not found")
 
-    protected val PromiseRejectOnExceptionBlock.offlineModule: OfflineModule get() = context.offlineModule
+    protected val RejectPromiseOnExceptionBlock.offlineModule: OfflineModule get() = context.offlineModule
         ?: throw IllegalStateException("OfflineModule not found")
 
-    fun PromiseRejectOnExceptionBlock.getPlayer(nativeId: NativeId): Player = playerModule.getPlayerOrNull(nativeId)
+    protected val RejectPromiseOnExceptionBlock.drmModule: DrmModule get() = context.drmModule
+        ?: throw IllegalStateException("DrmModule not found")
+
+    fun RejectPromiseOnExceptionBlock.getPlayer(nativeId: NativeId): Player = playerModule.getPlayerOrNull(nativeId)
         ?: throw IllegalArgumentException("Invalid PlayerId")
 }
 
 /** Run [block], forwarding the return value. If it throws, sets [Promise.reject] and return null. */
-inline fun <T> Promise.runAndRejectOnException(block: PromiseRejectOnExceptionBlock.() -> T): T? = try {
-    PromiseRejectOnExceptionBlock.block()
+inline fun <T> Promise.runAndRejectOnException(block: RejectPromiseOnExceptionBlock.() -> T): T? = try {
+    RejectPromiseOnExceptionBlock.block()
 } catch (e: Exception) {
     reject(e)
     null
 }
 
 /** Receiver of code that can safely throw when resolving a [Promise]. */
-object PromiseRejectOnExceptionBlock
+object RejectPromiseOnExceptionBlock
