598d8bfd83327882563d185c945839f61e0721b4
Kevin Rocard <kevin.rocard@bitmovin.com>
2023-11-14 14:24:36 +0100

    fix: factorize with* methods

--- android/src/main/java/com/bitmovin/player/reactnative/extensions/ReadableMapExtension.kt
+++ android/src/main/java/com/bitmovin/player/reactnative/extensions/ReadableMapExtension.kt
@@ -10,34 +10,40 @@ fun ReadableMap.getBooleanOrNull(
 inline fun <T> ReadableMap.withDouble(
     key: String,
     block: (Double) -> T,
-): T? = takeIf { hasKey(key) }?.getDouble(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getDouble, block)
 
 inline fun <T> ReadableMap.withMap(
     key: String,
     block: (ReadableMap) -> T,
-): T? = takeIf { hasKey(key) }?.getMap(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getMap, block)
 
 inline fun <T> ReadableMap.withInt(
     key: String,
     block: (Int) -> T,
-): T? = takeIf { hasKey(key) }?.getInt(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getInt, block)
 
 inline fun <T> ReadableMap.withBoolean(
     key: String,
     block: (Boolean) -> T,
-): T? = takeIf { hasKey(key) }?.getBoolean(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getBoolean, block)
 
 inline fun <T> ReadableMap.withString(
     key: String,
     block: (String) -> T,
-): T? = getString(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getString, block)
 
 inline fun <T> ReadableMap.withArray(
     key: String,
     block: (ReadableArray) -> T,
-): T? = getArray(key)?.let(block)
+): T? = mapValue(key, ReadableMap::getArray, block)
 
 inline fun <T> ReadableMap.withStringArray(
     key: String,
     block: (List<String?>) -> T,
-): T? = getArray(key)?.toStringList()?.let(block)
+): T? = mapValue(key, { getArray(it)?.toStringList() }, block)
+
+inline fun <T, R> ReadableMap.mapValue(
+    key: String,
+    get: ReadableMap.(String) -> T?,
+    block: (T) -> R,
+) = takeIf{ hasKey(key) }?.get(key)?.let(block)
