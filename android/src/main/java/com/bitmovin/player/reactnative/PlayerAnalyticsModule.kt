f32a92f1aa3462b5f2cda932c6a063dc522918d2
Kevin Rocard <kevin.rocard@bitmovin.com>
2023-12-04 10:50:31 +0100

    fix: reject if analytics is disable

--- android/src/main/java/com/bitmovin/player/reactnative/PlayerAnalyticsModule.kt
+++ android/src/main/java/com/bitmovin/player/reactnative/PlayerAnalyticsModule.kt
@@ -1,5 +1,6 @@
 package com.bitmovin.player.reactnative
 
+import com.bitmovin.player.api.analytics.AnalyticsApi
 import com.bitmovin.player.api.analytics.AnalyticsApi.Companion.analytics
 import com.bitmovin.player.reactnative.converter.toAnalyticsCustomData
 import com.facebook.react.bridge.*
@@ -17,25 +18,33 @@ class PlayerAnalyticsModule(context: ReactApplicationContext) : BitmovinBaseModu
     /**
      * Sends a sample with the provided custom data.
      * Does not change the configured custom data of the collector or source.
-     * @param nativeId Native Id of the collector instance.
+     * @param playerId Native Id of the player instance.
      * @param json Custom data config json.
      */
     @ReactMethod
-    fun sendCustomDataEvent(nativeId: NativeId, json: ReadableMap, promise: Promise) {
-        promise.unit.resolveOnUiThread {
-            getPlayer(nativeId).analytics?.sendCustomDataEvent(json.toAnalyticsCustomData())
+    fun sendCustomDataEvent(playerId: NativeId, json: ReadableMap, promise: Promise) {
+        promise.unit.resolveOnUiThreadWithAnalytics(playerId) {
+            sendCustomDataEvent(json.toAnalyticsCustomData())
         }
     }
 
     /**
      * Gets the current user Id for a player instance with analytics.
-     * @param nativeId Native Id of the the player instance.
+     * @param playerId Native Id of the the player instance.
      * @param promise JS promise object.
      */
     @ReactMethod
     fun getUserId(playerId: NativeId, promise: Promise) {
-        promise.string.nullable.resolveOnUiThread {
-            getPlayer(playerId).analytics?.userId
+        promise.string.resolveOnUiThreadWithAnalytics(playerId) {
+            userId
         }
+    }
+
+    private inline fun <T> TPromise<T>.resolveOnUiThreadWithAnalytics(
+        playerId: NativeId,
+        crossinline block: AnalyticsApi.() -> T,
+    ) = resolveOnUiThread {
+        val analytics = getPlayer(playerId).analytics ?: throw IllegalStateException("Analytics is disabled")
+        analytics.block()
     }
 }
