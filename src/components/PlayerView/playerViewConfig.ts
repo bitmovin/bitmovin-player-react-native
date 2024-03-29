d5dafe1322ea6a7bf4e0a2a439f5ff06d0b6b3c4
Daniel Weinberger <dweinber@users.noreply.github.com>
2024-03-29 10:24:23 +0100

    Remove superfluous '$'
    Co-authored-by: Roland Kákonyi <roland.kakonyi@bitmovin.com>

--- src/components/PlayerView/playerViewConfig.ts
+++ src/components/PlayerView/playerViewConfig.ts
@@ -22,7 +22,7 @@ export interface PlayerViewConfig {
 
   /**
    * When set to `true`, the first frame of the main content will not be rendered before playback starts. Default is `false`.
-   * This configuration has no effect for the ${@link UserInterfaceType.Subtitle} on iOS/tvOS.
+   * This configuration has no effect for the {@link UserInterfaceType.Subtitle} on iOS/tvOS.
    *
    * To reliably hide the first frame before a pre-roll ad, please ensure that you are using the {@link AdvertisingConfig} to schedule ads and not the {@link Player.scheduleAd} API call.
    */
