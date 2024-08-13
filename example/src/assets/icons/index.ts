e067de305d3692254a4d90f432e9611c1b79de12
saravanan <saravanan.silvarajoo@bitmovin.com>
2024-08-13 17:24:16 +0800

    assets for customui

---
+++ example/src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as VideoPlay } from './video-play.svg';
+export { default as VideoPause } from './video-pause.svg';
+export { default as VideoSkipBack } from './video-backward.svg';
+export { default as VideoSkipForward } from './video-forward.svg';
