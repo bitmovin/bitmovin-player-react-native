cbbaaeef5d815db0e8ee2bb9a2049c2b73bcc4fa
Michele Pozzi <123.mpozzi@gmail.com>
2024-10-07 16:13:09 +0200

    Add `LockScreenControlConfig` docs

--- src/lockScreenControlConfig.ts
+++ src/lockScreenControlConfig.ts
@@ -1,4 +1,8 @@
-// TODO: docs
+/**
+ * Configures the lock screen information for the application. This information will be displayed
+ * wherever lock screen information typically appears, such as the lock screen
+ * and inside the control center.
+ */
 export interface LockScreenControlConfig {
   // TODO: docs
   isEnabled?: boolean;
