5c04e05635191744ecd20b4a2ba2755a722871a4
Michele Pozzi <123.mpozzi@gmail.com>
2024-10-18 11:14:14 +0200

    Cleanup unnecessary code and comments

--- android/src/main/java/com/bitmovin/player/reactnative/services/MediaSessionPlaybackService.kt
+++ android/src/main/java/com/bitmovin/player/reactnative/services/MediaSessionPlaybackService.kt
@@ -9,9 +9,6 @@ import com.bitmovin.player.api.media.session.MediaSessionService
 
 class MediaSessionPlaybackService : MediaSessionService() {
     inner class ServiceBinder : Binder() {
-        // When the service starts, it creates a player
-        // When playback activity is created, it gets a binder and
-        // goes to the service and gets the player from it -- the same player instance.
         var player: Player?
             get() = this@MediaSessionPlaybackService.player
             set(value) {
@@ -35,18 +32,10 @@ class MediaSessionPlaybackService : MediaSessionService() {
 
     override fun onGetSession(): MediaSession? = mediaSession
 
-    // Player has 2 pointers: one from application, one from service
-    // but is the same instance.
-    // So when it loses the application one, it still does not get
-    // garbage-collected because it still has a strong reference.
     override fun onCreate() {
         super.onCreate()
-        // [!!] I need to get the same react-native instance of the player here
         val player = this.player ?: Player(this)
 
-        // cannot create mediaSession without a player
-        // TODO: call playerModule.createPlayer to actually create a player
-        // so then we'll go to `mediaSessionModule.. onServiceConnected`
         mediaSession = MediaSession(
             this,
             mainLooper,
@@ -71,7 +60,7 @@ class MediaSessionPlaybackService : MediaSessionService() {
     private fun createMediaSession(player: Player) {
         binder.disconnectSession()
 
-        val newMediaSession = MediaSession(// the real media session
+        val newMediaSession = MediaSession(
             this,
             mainLooper,
             player,
