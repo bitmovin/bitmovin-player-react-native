c012ece2b10bf0d0701d4b4653b41d9ba0c22152
Lucas <lucas.gruenberger@bitmovin.com>
2025-03-06 14:54:15 +0100

    Add PlaybackConfig.decoderConfig

---
+++ src/decoderConfig.ts
@@ -0,0 +1,36 @@
+/**
+ * Configures the playback behaviour of the player.
+ *
+ @platform Android
+ */
+export interface DecoderConfig {
+  decoderPriorityProvider?: DecoderPriorityProvider;
+}
+
+/**
+ * Configures the playback behaviour of the player.
+ *
+ @platform Android
+ */
+export interface DecoderPriorityProvider {
+  overrideDecodersPriority(
+    context: DecoderContext,
+    preferredDecoders: [MediaCodecInfo]
+  ): [MediaCodecInfo];
+}
+
+/** The context in which a new decoder is chosen. */
+export interface DecoderContext {
+  mediaType: DecoderContextMediaType;
+  isAd: boolean;
+}
+
+export interface MediaCodecInfo {
+  name: String;
+  isSoftware: boolean;
+}
+
+export enum DecoderContextMediaType {
+  AUDIO = 'audio',
+  VIDEO = 'video',
+}
