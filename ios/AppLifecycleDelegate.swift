4c4fc383a28cf0993eb44086471911d4926e76d5
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2025-06-24 16:19:40 +0200

    fix tvOS build

--- ios/AppLifecycleDelegate.swift
+++ ios/AppLifecycleDelegate.swift
@@ -4,11 +4,15 @@ import ExpoModulesCore
 public class AppLifecycleDelegate: ExpoAppDelegateSubscriber {
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // TODO: Add support for OfflineConfig via config plugin
+#if os(iOS)
         OfflineManager.initializeOfflineManager()
+#endif
         return true
     }
 
     public func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+#if os(iOS)
         OfflineManager.sharedInstance().add(completionHandler: completionHandler, for: identifier)
+#endif
     }
 }
