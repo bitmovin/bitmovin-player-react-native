066e7cd6fb1d02767bb0b5c565bf69b7e8d145fc
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2025-07-17 22:36:50 +0200

    fix integration test environment variable handling

--- integration_test/scripts/stop-packager.sh
+++ integration_test/scripts/stop-packager.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 ps aux | grep "$(pwd)" | grep "integration_test" | grep "start" | awk '{print $2}' | xargs -r kill
+
+# kill process using port 8082 as well
+lsof -ti:8082 | xargs -r kill
+
 exit 0
