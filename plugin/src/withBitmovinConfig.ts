38c56fd7b3f63a90ab796af4c76271350d1b1b46
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2025-08-01 21:23:14 +0200

    add config plugin example

--- plugin/src/withBitmovinConfig.ts
+++ plugin/src/withBitmovinConfig.ts
@@ -11,6 +11,34 @@ const defaultFeatureFlags: FeatureFlags = {
   pictureInPicture: false,
 };
 
+/**
+ * Expo Config Plugin for Bitmovin Player.
+ * This plugin configures the Bitmovin Player for both iOS and Android platforms.
+ * It accepts a player license key and feature flags to customize the player's behavior.
+ * @param config - The Expo config object.
+ * @param options - An object containing the player license key and feature flags.
+ * @returns The modified Expo config object with Bitmovin Player configurations.
+ * 
+ * @example
+ * // app.config.js
+ * module.exports = {
+ *   plugins: [
+ *     [
+ *       'bitmovin-player-react-native',
+ *       {
+ *         playerLicenseKey: 'YOUR_BITMOVIN_PLAYER_LICENSE_KEY',
+ *         featureFlags: {
+ *           airPlay: true,
+ *           backgroundPlayback: true,
+ *           googleCastSDK: { android: '21.3.0', ios: '4.8.1.2' },
+ *           offline: true,
+ *           pictureInPicture: true,
+ *         },
+ *       },
+ *     ]
+ *   ]
+ * };
+ */
 const withBitmovinConfig: ConfigPlugin<{
   playerLicenseKey: string;
   featureFlags: FeatureFlags;
