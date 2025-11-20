5b921b70e804ff70d083eada94d879da1629b44f
Roland Kákonyi <roland.kakonyi@bitmovin.com>
2025-11-20 10:40:13 +0100

    improve iOS/tvOS build pipelines

--- integration_test/scripts/build-ios.sh
+++ integration_test/scripts/build-ios.sh
@@ -9,6 +9,7 @@ XCBEAUTIFY_ARGS=$@
 eval "xcodebuild -workspace ios/IntegrationTest.xcworkspace \
     -scheme IntegrationTest \
     -configuration Debug \
+    -destination 'generic/platform=iOS Simulator' \
     -quiet \
     ${XCODEBUILD_ARGS} \
     | xcbeautify -qq --disable-logging $XCBEAUTIFY_ARGS"
