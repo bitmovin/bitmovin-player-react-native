64bbab8010b12a6f3e4f7deb2aa7c44a853ecba4
Roland Kákonyi <roland.kakonyi@bitmovin.com>
2025-11-20 11:17:46 +0100

    improve integration test setup

--- integration_test/scripts/start-test-ios.sh
+++ integration_test/scripts/start-test-ios.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Start iOS test by finding available iPhone simulator and executing cavy
 
+ARGS=$@
+
 # Get the first available iPhone simulator name
 SIMULATOR_NAME=$(xcrun simctl list devices available -e -j | \
     jq --raw-output -s '[.[].devices.[].[] | select( .deviceTypeIdentifier | contains("iPhone") )|.name] | first')
 
 if [ -n "$SIMULATOR_NAME" ] && [ "$SIMULATOR_NAME" != "null" ]; then
     echo "Running tests on iOS simulator: $SIMULATOR_NAME"
-    yarn cavy run-ios --simulator "$SIMULATOR_NAME"
+    yarn cavy run-ios --simulator "$SIMULATOR_NAME" --terminal "terminal" $ARGS
 else
     echo "No iPhone simulator available"
     exit 1
