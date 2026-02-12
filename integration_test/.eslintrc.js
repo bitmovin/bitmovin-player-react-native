8f077a70ae5a29e8f943a3efaa5c493bb468978e
Michele Pozzi <123.mpozzi@gmail.com>
2026-02-12 15:06:51 +0100

    Fix ESLint not being able to run

--- integration_test/.eslintrc.js
+++ integration_test/.eslintrc.js
@@ -2,10 +2,20 @@ module.exports = {
   root: true,
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
   env: {
     node: true,
   },
   rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
     'prettier/prettier': 'error',
   },
 };
