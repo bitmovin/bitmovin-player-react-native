e2ef4f503ead414c668997332364705b3c043e37
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2026-02-26 10:50:23 +0100

    Add integration test for DeficiencyData on DRM license errors
    Load a DRM source with a broken license URL (403) and verify the
    resulting error event contains httpResponse with status, url, and
    request details.

--- integration_test/tests/index.ts
+++ integration_test/tests/index.ts
@@ -1,6 +1,7 @@
 import AdvertisingTest from './advertisingTest';
 import audioTrackTest from './audioTrackTest';
 import CaptionTest from './captionTest';
+import ErrorTest from './errorTest';
 import LoadingTest from './loadingTest';
 import metadataId3Test from './metadataId3Test';
 import PlaybackTest from './playbackTest';
@@ -9,6 +10,7 @@ import UnloadingTest from './unloadingTest';
 export default [
   AdvertisingTest,
   CaptionTest,
+  ErrorTest,
   LoadingTest,
   metadataId3Test,
   PlaybackTest,
