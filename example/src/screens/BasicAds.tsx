e4011aacd28953c3e29416f441c69664fb065a41
Michele Pozzi <123.mpozzi@gmail.com>
2026-03-02 16:56:40 +0100

    Remove commented out vast tag

--- example/src/screens/BasicAds.tsx
+++ example/src/screens/BasicAds.tsx
@@ -29,7 +29,6 @@ const createAdTags = () => ({
   ),
   vast2: withCorrelator(
     'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&correlator='
-    // 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonly&cmsid=496&vid=short_onecue&correlator='
   ),
 });
 
